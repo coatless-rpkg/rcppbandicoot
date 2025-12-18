e5c85f308179b51bc699bf508732bad7722eb76f
Dirk Eddelbuettel <edd@debian.org>
2025-12-17 19:26:20 -0600

    Clean up .onLoad() removing commented-out code

--- R/rcppbandicoot-package.R
+++ R/rcppbandicoot-package.R
@@ -8,25 +8,6 @@
 NULL
 
 .onLoad <- function(libname, pkgname) {
-  # Set the kernel path for Bandicoot OpenCL kernels
-  #kernel_path <- system.file("include/bandicoot_bits/opencl/kernels",
-  #                          package = "RcppBandicoot")
-
-  #if (nzchar(kernel_path)) {
-  #  Sys.setenv(COOT_CL_KERNEL_PATH = kernel_path)
-  #}
-
-  #cuda_path <- system.file("include/bandicoot_bits/cuda/kernels",
-  #                         package = "RcppBandicoot")
-  #if (nzchar(cuda_path)) {
-  #  Sys.setenv(COOT_CUDA_KERNEL_PATH = kernel_path)
-  #}
-
-    #kernel_path <- system.file("include/bandicoot_bits/", package = "RcppBandicoot")
-    #cat("kernel_path: ", kernel_path, "\n")
-    #if (nzchar(kernel_path)) {
-    #    Sys.setenv(COOT_KERNEL_SOURCE_DIR = kernel_path)
-    #}
-
-    #gpu_initialize("cuda", TRUE)
+    ## this function is now empty as the kernel path is a _compile-time_
+    ## and not run-time issue
 }
